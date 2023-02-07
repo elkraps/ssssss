ca51b2c8e495e07e50f360b71d1c579342b147d9
Евгения <ilinaevgeniya@mail.ru>
2023-02-07 20:05:45 +0300

    8

--- client/src/components/DeviceList.js
+++ client/src/components/DeviceList.js
@@ -1,18 +1,21 @@
 import React, {useContext} from 'react';
 import {observer} from "mobx-react-lite";
 import {Context} from "../index";
-import {Row} from "react-bootstrap";
+import {Form} from "react-bootstrap";
 import DeviceItem from "./DeviceItem";
+import './components.css';
 
 const DeviceList = observer(() => {
     const {device} = useContext(Context)
 
     return (
-        <Row className="d-flex">
+        
+        <Form className='device-list'>
             {device.devices.map(device =>
                 <DeviceItem key={device.id} device={device}/>
             )}
-        </Row>
+        </Form>
+        
     );
 });
 
